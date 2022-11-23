15e454fd10e70d67c35e11a365e1c66eb46f5250
CreativeHazio <ezekieldamin021@gmail.com>
2022-11-23 12:47:16 -1100

    Added a basic splash screen

---
+++ app/src/main/java/com/creativehazio/notepadapp/activities/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.creativehazio.notepadapp.activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.creativehazio.notepadapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
